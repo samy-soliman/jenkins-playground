7c03fa35e1530cbd8294aa45cad46df08419a67b
samy-soliman <42540697+samy-soliman@users.noreply.github.com>
2023-09-23 00:18:52 +0300

    Update and rename test.groovy to install.groovy

---
+++ vars/install.groovy
@@ -0,0 +1,36 @@
+def call()  {
+  pipeline {
+    agent { 
+        label 'worker1' 
+    }
+    
+    stages {
+        stage('Install Nginx') {
+            steps {
+                script {
+                  sh 'apt-get update'
+                  sh 'apt-get install -y nginx'
+                  echo 'installed nginx'
+                }
+            }
+        }
+        
+        stage('Start Nginx') {
+            steps {
+                script {
+                  sh 'service nginx start'
+                  echo 'started nginx'
+                }
+            }
+        }
+    }
+    
+    post {
+        always {
+            echo 'End nginx pipeline'
+        }
+    }
+}
+
+}
+  
